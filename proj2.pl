13df430635f9fa6bc1045541c808d36e317c8e65
dianamart3 <dmartinez3399@gmail.com>
2025-05-11 16:11:51 -0500

    simulationg of going off in a path given specific coordinates

--- proj2.pl
+++ proj2.pl
@@ -16,3 +16,9 @@ move(coord(R,C), down, coord(R2,C))  :- R2 is R + 1.
 move(coord(R,C), left, coord(R,C2))  :- C2 is C - 1.
 move(coord(R,C), right, coord(R,C2)) :- C2 is C + 1.
 
+simulate(_, Coord, [], Coord). % no more actions
+simulate(Map, Coord, [Dir|Rest], FinalCoord) :-
+    move(Coord, Dir, NewCoord),
+    NewCoord = coord(R,C),
+    valid_cell(Map, R, C),
+    simulate(Map, NewCoord, Rest, FinalCoord).
