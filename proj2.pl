5859c2fe3b944a962f558dcd86233b3bc02eb4ff
dianamart3 <dmartinez3399@gmail.com>
2025-05-11 14:56:54 -0500

    5-11 2:56 ensures move stays within the walls

--- proj2.pl
+++ proj2.pl
@@ -4,3 +4,8 @@ get(List, Row, Col, Value) :-
 find_position(Map, Symbol, RowIndex, ColIndex) :-
     nth0(RowIndex, Map, Row),
     nth0(ColIndex, Row, Symbol).
+
+valid_cell(Map, Row, Col) :-
+    nth0(Row, Map, RowList),
+    nth0(Col, RowList, Cell),
+    Cell \= w.
