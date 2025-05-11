d06d51dd637e4eb63f678b511e810d83709c6f3f
dianamart3 <dmartinez3399@gmail.com>
2025-05-11 00:09:45 -0500

    [5-11 12:09] finding coordinates

--- proj2.pl
+++ proj2.pl
@@ -1,3 +1,6 @@
 get(List, Row, Col, Value) :-
     nth0(Row, List, RowList),
     nth0(Col, RowList, Value).
+find_position(Map, Symbol, RowIndex, ColIndex) :-
+    nth0(RowIndex, Map, Row),
+    nth0(ColIndex, Row, Symbol).
